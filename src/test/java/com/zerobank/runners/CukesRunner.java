189a2926b8e95dc86e1e51b325aecfeff48e5e3e
Pazilye <pazi.jurat@outlook.com>
2023-11-13 22:11:08 +0100

    Runner class created, and failed test runner class added

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -1,6 +1,30 @@
 package com.zerobank.runners;
 
-public class CukesRunner {
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "json:target/cucumber-reports.json",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        tags = ""
+
+
+
+
+)
+
+
+
+public class CukesRunner {
 
 }
