cf27c729370b50b7c706462d150700959a3c1c23
Pazilye <pazi.jurat@outlook.com>
2023-11-15 21:16:16 +0100

    Locators added to the LoginPage

--- src/test/java/com/zerobank/pages/LoginPage.java
+++ src/test/java/com/zerobank/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package com.zerobank.pages;
 
 import com.zerobank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
@@ -8,6 +10,17 @@ public class LoginPage {
     public LoginPage (){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(id = "user_login")
+    public WebElement LoginInput;
+
+    @FindBy(id = "user_password")
+    public WebElement PasswordInput;
+
+    @FindBy(xpath = "//input[@name='submit']")
+    public WebElement SigninButton;
+
+
 
 
 
