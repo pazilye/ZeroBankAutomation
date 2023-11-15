89c6fb55d2af9bc3506ef1c76007b2d656fd8ca4
Pazilye <pazi.jurat@outlook.com>
2023-11-15 22:43:02 +0100

    Homepage class updated with a new locator

--- src/test/java/com/zerobank/pages/HomePage.java
+++ src/test/java/com/zerobank/pages/HomePage.java
@@ -1,6 +1,18 @@
 package com.zerobank.pages;
 
+import com.zerobank.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
+
+    public HomePage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//div[@id='settingsBox']/ul/li[2]/a/i")
+    public WebElement SettingsButton;
 
 
 }
