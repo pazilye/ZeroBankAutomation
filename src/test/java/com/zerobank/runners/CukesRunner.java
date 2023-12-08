88bb663cf3954e18b88aa8713e44c86d742ef153
Pazilye <pazi.jurat@outlook.com>
2023-12-09 00:39:18 +0100

    Cukes runner updated

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions",
+        glue = "com/zerobank/stepdefinitions",
         dryRun = false,
         tags = ""
 
