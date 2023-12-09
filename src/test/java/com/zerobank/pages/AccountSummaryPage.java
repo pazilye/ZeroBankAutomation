596481918dc9c07aa04562eb8c39562ac9cd98c9
Pazilye <pazi.jurat@outlook.com>
2023-12-09 21:59:33 +0100

    New webElements are located in the AccountSummaryPage

--- src/test/java/com/zerobank/pages/AccountSummaryPage.java
+++ src/test/java/com/zerobank/pages/AccountSummaryPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import javax.xml.xpath.XPath;
+
 public class AccountSummaryPage {
 
     public AccountSummaryPage(){
@@ -19,6 +21,37 @@ public class AccountSummaryPage {
 
     @FindBy(xpath = "(//table[@class='table'])[3]/thead/tr/th")
     public WebElement ColumnNames;
+
+    @FindBy(xpath = "((//table[@class='table'])[1]/tbody/tr/td/a)[1]")
+    public WebElement SavingsBtn;
+
+    @FindBy(xpath = "(//table[@class='table'])[2]/tbody/tr/td/a")
+    public WebElement BrokeageBtn;
+
+    @FindBy(xpath = "((//table[@class='table'])[3]/tbody/tr/td)[1]/a")
+    public WebElement CheckingBtn;
+
+    @FindBy(xpath = "(//table)[3]/tbody/tr[2]/td[1]/a")
+    public WebElement CreditCardBtn;
+
+    @FindBy(xpath = "(//table[@class='table'])[4]/tbody/tr/td[1]/a")
+    public WebElement LoanBtn;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
